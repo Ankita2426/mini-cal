5195dc3e13df43ee038a80583dca06b7e0e81608
Ankita2426 <ankitakapoor1526@gmail.com>
2020-05-09 21:43:57 +0530

    love calculator

---
+++ Love_cal.js
@@ -0,0 +1,5 @@
+prompt("what is your name?(PERSON1) ");
+prompt("what is your name?(PERSON2) ");
+var lovescore = Math.random() * 100;
+lovescore = Math.floor(lovescore)+1;
+alert("your lovescore is " + lovescore);
