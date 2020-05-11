20948fb09f3c34db9000afcd5ecd7397f05f0460
Ankita2426 <ankitakapoor1526@gmail.com>
2020-05-11 07:52:53 +0530

    final

--- Love_cal.js
+++ Love_cal.js
@@ -1,5 +1,15 @@
 prompt("what is your name?(PERSON1) ");
 prompt("what is your name?(PERSON2) ");
+
 var lovescore = Math.random() * 100;
+
 lovescore = Math.floor(lovescore)+1;
-alert("your lovescore is " + lovescore);
+if(lovescore>70){
+  alert("your lovescore is "+ lovescore + "% ." + " you love each other unconditionally");
+}
+if(lovescore > 30 && lovescore <= 70){
+  alert(" your lovescore is "+ lovescore + "% .");
+}
+if(lovescore <= 30){
+  alert(" your lovescore is "+ lovescore + "% ." + " you love each other like iol and water.")
+}
